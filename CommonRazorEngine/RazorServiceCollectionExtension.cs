0fcb0e40b3c99c6274636d02f88fbc47b208d4c8
Alin <alincosmin@gmail.com>
2023-01-09 10:16:25 +0200

    Add AddRazorRuntimeCompilation and FileProvider

--- CommonRazorEngine/RazorServiceCollectionExtension.cs
+++ CommonRazorEngine/RazorServiceCollectionExtension.cs
@@ -1,15 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.CodeAnalysis.Emit;
+﻿using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CommonRazorEngine
 {
@@ -20,12 +12,12 @@ namespace CommonRazorEngine
         /// </summary>
         public static void AddCommonRazorEngine(this IServiceCollection services, IConfiguration configuration)
         {
-            //var fileProvider = new EmbeddedFileProvider(typeof(RazorViewToStringRenderer).Assembly);
+            var fileProvider = new EmbeddedFileProvider(typeof(RazorViewToStringRenderer).Assembly);
 
-            // FileProviders property is not available anymore           
-            services.Configure<RazorViewEngineOptions>(options =>
+            services.Configure<MvcRazorRuntimeCompilationOptions>(options =>
             {
-                //options.FileProviders.Add(fileProvider);
+                options.FileProviders.Clear();
+                options.FileProviders.Add(fileProvider);
             });
 
             services.AddScoped<IRazorViewToStringRenderer, RazorViewToStringRenderer>();
--- WebJobConsoleApp/Program.cs
+++ WebJobConsoleApp/Program.cs
@@ -1,17 +1,15 @@
 ﻿using CommonRazorEngine;
 using CommonRazorEngine.Views;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Routing.Template;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.ObjectPool;
-using Microsoft.Extensions.PlatformAbstractions;
+using System;
 using System.Diagnostics;
 using System.Reflection;
-
+using System.Threading.Tasks;
 
 namespace WebJobConsoleApp
 {
@@ -28,18 +26,6 @@ namespace WebJobConsoleApp
             ServiceCollection services = ConfigureServices();
             ServiceProvider = services.BuildServiceProvider();
 
-            var builder = new HostBuilder();
-            builder.ConfigureWebJobs(b =>
-            {
-                //b.AddAzureStorageCoreServices();
-            });
-
-            var host = builder.Build();
-            using (host)
-            {
-              //await host.RunAsync();
-            }
-
             // Test razor view engine
             var viewToStringEngine = ServiceProvider.GetService<IRazorViewToStringRenderer>();
             string htmlContent = await viewToStringEngine.RenderToStringAsync<MyView>("~/Views/MyView.cshtml", new MyView());
@@ -52,22 +38,21 @@ namespace WebJobConsoleApp
             var services = new ServiceCollection();
             services.AddSingleton<IConfiguration>(Configuration);
 
-            var applicationEnvironment = PlatformServices.Default.Application;
-            services.AddSingleton(applicationEnvironment);
-
             services.AddSingleton<Microsoft.AspNetCore.Hosting.IWebHostEnvironment>(new WebJobHostEnvironment
             {
                 ApplicationName = Assembly.GetEntryAssembly().GetName().Name,
-                //ApplicationName = typeof(IRazorViewToStringRenderer).Assembly.GetName().Name,
             });
 
             var listener = new DiagnosticListener("Microsoft.AspNetCore");
             services.AddSingleton<DiagnosticListener>(listener);
             services.AddSingleton<DiagnosticSource>(listener);
+
             services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
             services.AddSingleton<ILoggerFactory, LoggerFactory>(sp => new LoggerFactory());
 
-            services.AddMvcCore().AddRazorViewEngine();
+            services.AddMvcCore()
+                    .AddRazorViewEngine()
+                    .AddRazorRuntimeCompilation();
 
             services.AddCommonRazorEngine(Configuration);
 
